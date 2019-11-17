fbeccd3dcc861535a7725d2954d9b3581ef75092
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-11-17 18:11:00 +0100

    Seccion 3 terminada.

---
+++ src/observbles/03-subject.ts
@@ -0,0 +1,51 @@
+
+import { Observable, Observer, Subscriber, Subject, Subscription } from 'rxjs';
+import { setTimeout, setInterval } from 'timers';
+
+const observer: Observer<any> = {
+
+    next: value => console.log( 'next: ', value ),
+    error: error => console.warn( 'error: ', error),
+    complete: () => console.info( 'completado' )
+
+}
+
+const intervalo$ = new Observable<number>( subs => {
+
+    const intervalID = setInterval( () => {
+        subs.next( Math.random() );
+    }, 1000);
+
+    return () => {
+        clearInterval( intervalID );
+        console.log('Intervalo destruido');
+    };
+
+});
+
+/**
+ * 1- Casteo múltiple. Distribuye la misma información a todoas las suscripciones. 
+ * 2- También es un observer. 
+ * 3- next, error, complete.
+ */
+const subject$ = new Subject();
+const subscription = intervalo$.subscribe( subject$ );
+
+// const subs1 = intervalo$.subscribe( rnd => console.log( 'subs1', rnd ) );
+// const subs2 = intervalo$.subscribe( rnd => console.log( 'subs2', rnd ) );
+
+// const subs1 = subject$.subscribe( rnd => console.log( 'subs1', rnd ) );
+// const subs2 = subject$.subscribe( rnd => console.log( 'subs2', rnd ) );
+
+const subs1 = subject$.subscribe( observer );
+const subs2 = subject$.subscribe( observer );
+
+setTimeout( () => {
+
+    subject$.next( 10 );
+
+    subject$.complete();
+
+    subscription.unsubscribe();
+
+}, 3500);
