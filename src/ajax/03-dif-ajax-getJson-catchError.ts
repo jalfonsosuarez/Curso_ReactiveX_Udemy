5ed84e5699fd4ed0b644061d0a2605710c6216d0
josealfonso.suarez <joseasuarez@gmail.com>
2019-11-21 11:57:15 +0100

    Seccion 8 terminada

---
+++ src/ajax/03-dif-ajax-getJson-catchError.ts
@@ -0,0 +1,38 @@
+import { of } from 'rxjs';
+import { ajax, AjaxError } from 'rxjs/ajax';
+import { catchError } from 'rxjs/operators';
+
+
+const url = 'https://httpbinX.org/delay/1';
+// const url = "https://api.github.com/users?per_page=5";
+
+const manejaError = ( resp: AjaxError ) => {
+    console.warn( 'error', resp.message );
+    return of( {
+        ok: false,
+        usuarios: []
+    });
+}
+
+// const obs$ = ajax.getJSON( url ).pipe(
+//     catchError( manejaError )
+// );
+// const obs2$ = ajax( url ).pipe(
+//     catchError( manejaError )
+// );
+
+const obs$ = ajax.getJSON( url );
+const obs2$ = ajax( url );
+
+// obs$.subscribe( data => console.log( 'getJson:', data ) );
+obs$.pipe(
+    catchError( manejaError )
+).subscribe({
+    next: val => console.log( 'next', val ),
+    error: err => console.warn( 'error:', err ),
+    complete: () => console.log( 'complete ')
+} );
+
+
+// obs2$.subscribe( data => console.log( 'ajax:', data ) );
+
